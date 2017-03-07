4bda04736e3c855048ed69e0fc9cbe9a4d4e4fdd
Roland Mosimann <roland.mosimann@students.fhnw.ch>
2017-03-07 18:06:22 +0100

    added methods

--- src/ch/fhnw/SPN.java
+++ src/ch/fhnw/SPN.java
@@ -4,13 +4,24 @@ package ch.fhnw;
  * Created by pwg on 07.03.17.
  */
 public class SPN {
-  String key;
-  String chiffre;
+    String key;
+    String chiffre;
 
-  public SPN(String key, String chiffre){
-    this.key= key;
-    this.chiffre=chiffre;g
+    public SPN(String key, String chiffre){
+        this.key= key;
+        this.chiffre=chiffre;
+    }
 
-  }
+    private String sBox(String x){
+        return new String("");
+    }
+
+    private String bitPermutation(String x){
+        return new String("");
+    }
+
+    private String xor(String x, String y){
+        return new String("");
+    }
 
 }
