cacc0ac43d419f869f81380e3eb43fb77dc1f6db
Patrick Wigger <patrick.wigger@students.fhnw.ch>
2017-03-07 17:58:12 +0100

    Class spn

--- src/ch/fhnw/Appstarter.java
+++ src/ch/fhnw/Appstarter.java
@@ -2,14 +2,19 @@ package ch.fhnw;
 
 public class Appstarter {
 
-    static final byte[] key = "00111010100101001101011000111111".getBytes();
-    static final byte[] chiffre = "00000100110100100000101110111000000000101000111110001110011111110110000001010001010000111010000000010011011001110010101110110000".getBytes();
+  static final String key = "00111010100101001101011000111111";
+  static final String chiffre = "00000100110100100000101110111000000000101000111110001110011111110110000001010001010000111010000000010011011001110010101110110000";
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+
+    SPN spn = new SPN(key, chiffre);
 
 
-        for (byte b:key) {
-            System.out.print(Integer.toBinaryString(b));}
+    }
 
-        }
+  }
+
+
+
+
 }
--- src/ch/fhnw/SPN.java
+++ src/ch/fhnw/SPN.java
@@ -4,5 +4,13 @@ package ch.fhnw;
  * Created by pwg on 07.03.17.
  */
 public class SPN {
+  String key;
+  String chiffre;
+
+  public SPN(String key, String chiffre){
+    this.key= key;
+    this.chiffre=chiffre;g
+
+  }
 
 }
